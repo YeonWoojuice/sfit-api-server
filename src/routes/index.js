4622e33f9d369cbb15edc14fabfbd768588eed91
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-11-11 15:06:26 +0900

    style: standardize quotation marks in route definitions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
-const { Router } = require('express');
-const healthRouter = require('./health.route');
+const { Router } = require("express");
+const healthRouter = require("./health.route");
 
 const router = Router();
 
-router.use('/health', healthRouter);
+router.use("/health", healthRouter);
 
 module.exports = router;
